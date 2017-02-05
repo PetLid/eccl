ca2caf9e61489188638491c277a57aeaeab98e84
PetLid <pelle@lidholm.se>
2017-02-05 23:57:54 +0100

    Remove console.log

--- src/base/gameWrapper.js
+++ src/base/gameWrapper.js
@@ -77,7 +77,6 @@ export default function() {
 
         while ( accumulator >= dt )
         {
-            console.log(accumulator);
             update(dt);
             accumulator -= dt;
             t += dt;
